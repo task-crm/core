d9bd35a61467db100751fa3037b58c7f083ac495
Jababite <tro.nick1997@yandex.ru>
2023-11-12 19:57:36 +0300

    first commit

---
+++ src/test/java/ru/sop/crm/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sop.crm.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
